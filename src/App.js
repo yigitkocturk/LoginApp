da3515028e53ea49a37adc778ca46db7ef1655a2
yigitkocturk <yigitkocturkk@gmail.com>
2022-10-26 17:35:15 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+
+
 function App() {
+
+  const [form, setForm] = useState({ name: '', city: '', birthday: '', adress: '' })
+
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Bilgilerinizi Giriniz</h2>
+      <form>
+        <input type="text" name="name" value={form.name} onChange={handleChange} placeholder="İsim Soyisim Giriniz..." />
+        <select name="city" value={form.city} onChange={handleChange}>
+          <option value="">Lütfen Şehir Seçiniz</option>
+          <option value="Kırklareli">Kırklareli</option>
+          <option value="Edirne">Edirne</option>
+          <option value="Tekirdağ">Tekirdağ</option>
+          <option value="İstanbul">İstanbul</option>
+        </select>
+        <input type="date" name="birthday" value={form.birthday} onChange={handleChange} />
+        <textarea name="adress" placeholder="Adresinizi giriniz" value={form.adress} onChange={handleChange}></textarea>
+        <button>Kaydet</button>
+      </form>
+      {form.name !== '' && (
+        <div>
+        <h3>Kayıt Olan Kişi Bilgileri</h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">City</th>
+              <th scope="col">Birthday</th>
+              <th scope="col">Adress</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+                <tr>
+                  <th scope="row"></th>
+                  <td>{form.name}</td>
+                  <td>{form.city}</td>
+                  <td>{form.birthday}</td>
+                  <td>{form.adress}</td>
+                 
+                </tr>
+            
+          </tbody>
+        </table>
+      </div>
+      )}
+      
     </div>
   );
 }
